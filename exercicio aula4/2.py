debf73ecac272655c7d42aa96be3a8c5bd507778
luan lima <luanlima04012002@icloud.com>
2025-07-18 17:23:36 -0300

    Add files via upload

---
+++ exercicio aula4/2.py
@@ -0,0 +1,10 @@
+n = float(input('Digite um número: '))
+
+if n == 0:
+    print(f'ZERO')
+
+elif n > 0:
+    print(f'{n} é +')
+
+elif n < 0:
+    print(f'{n} é -')
---
+++ exercicio aula4/4.py
@@ -0,0 +1,11 @@
+#4. Faça um Programa que verifique se uma letra digitada é vogal ou consoante.
+
+L = input('Digite alguma letra: ')
+
+
+if L in "a, e, i, o, u":
+    print(f'{L} é um vogal! ')
+
+else:
+    print(f'{L} é uma consoante! ')
+    
